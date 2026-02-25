c3b91e3d19aa86c6ea1709d22d3d93a7dafe7f03
Gian Raphael <gianraphaelbaldovino@gmail.com>
2026-02-25 19:02:01 +0800

    Update changes

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function QuoteBox() {
       <p id="text" className="text-center m-0 h2">{quote}</p>
       <p id="author" className="text-end m-0">{author}</p>
 
-      <a id="tweet-quote" href="twitter.com/intent/tweet" target="_top" className="btn btn-light p-1">
+      <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_top" className="btn btn-light p-1">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
           <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
         </svg>
